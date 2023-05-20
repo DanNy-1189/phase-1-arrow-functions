582d78dee4effd3d30d8af60f5d326f92b46f8bf
Nancy D. Arena <ndarena11@gmail.com>
2023-05-19 22:48:31 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,10 @@
+function divide(a, b) {
+    a = 2000
+    b = 100
+    let divide = (a/b);
+    return divide;
+};
+
+const square = parameter => parameter * parameter;
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
